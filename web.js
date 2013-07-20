7a22e0b223e854ed083a32a5a0f30f57dcf07ac5
Zeshan Khaled <zeshankhaled@yahoo.co.uk>
2013-07-20 16:05:45 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = fs.readFile('index.html');
+  response.send(buf.toString);
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
