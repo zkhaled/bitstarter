4455039eb635361767da95525af54f98b0de5fb8
Zeshan Khaled <zeshankhaled@yahoo.co.uk>
2013-07-20 18:38:13 +0000

    blah

--- web.js
+++ web.js
@@ -4,14 +4,8 @@ var fs = require('fs');
 
 app.use(express.logger());
 
-var str = fs.readFileSync('index.html', {encoding:'utf-8'});
-console.log('ZK str is '+str);
-
-var files = fs.readdirSync('.');
-console.log('files -> ' + files.join(' '));
-
 app.get('/', function(request, response) {
-  response.send('Blah');
+  response.send(fs.readFileSync('index.html', {encoding:'utf-8'}));
 });
 
 var port = process.env.PORT || 5000;
