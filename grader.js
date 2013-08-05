50543a7d0c13be3d0a6b8cd40aac0e3eb04d0b11
Zeshan Khaled <zeshankhaled@yahoo.co.uk>
2013-08-05 16:29:26 +0000

    checking in latest

--- grader.js
+++ grader.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
+var URL_DEFAULT = "";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -15,16 +17,27 @@ var assertFileExists = function(infile) {
 	return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-	return cheerio.load(fs.readFileSync(htmlfile));
+var assertURLExists = function(url) {
+	rest.get(url).on('complete', function(result) {
+		if (result instanceof Error) {
+			console.log("%s does not exist. Exiting.", url);
+			process.exit(1);
+		} else {
+			return url;
+		}
+	});
+}
+
+var cheerioHtml = function(html) {
+	return cheerio.load(html);
 };
 
 var loadChecks = function(checksfile) {
 	return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-	$ = cheerioHtmlFile(htmlfile);
+var checkHtml = function(html, checksfile) {
+	$ = cheerioHtml(html);
 	var checks = loadChecks(checksfile).sort();
 	var out = {};
 	for (var ii in checks) {
@@ -38,14 +51,29 @@ var clone = function(fn) {
 	return fn.bind({});
 };
 
+var validateHtml = function(html, checkfile) {
+
+	var checkJson = checkHtml(html, checkfile);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+}
+
 if (require.main == module) {
 	program
 		.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 		.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'URL of html file', null, URL_DEFAULT)
 		.parse(process.argv);
-	var checkJson = checkHtmlFile(program.file, program.checks);
-	var outJson = JSON.stringify(checkJson, null, 4);
-	console.log(outJson);
+
+	var html = '';
+	if (program.url == URL_DEFAULT) {
+		html = fs.readFileSync(program.file);
+		validateHtml(html,program.checks);
+	} else {
+		rest.get(program.url).on('complete', function(result) {
+			validateHtml(result,program.checks);
+		});
+	}
 } else {
 	exports.checkHtmlFile = checkHtmlFile;
 }
